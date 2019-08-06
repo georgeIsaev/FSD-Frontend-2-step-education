8fe225e1d5b18bae8f4c36628bba4c8f2109c582
Gevorg <georgeisaev7@gmail.com>
2019-08-06 23:44:45 +0400

    Install item-quantity-dropdown plugin, add  mixin for creating  quantity-dropdown

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import '../node_modules/jquery.maskedinput/src/jquery.maskedinput'
 // air-datepicker
 import '../node_modules/air-datepicker/dist/js/datepicker.min.js'
 import '../node_modules/air-datepicker/dist/css/datepicker.min.css'
+// item-quantity-dropdown
+import '../node_modules/item-quantity-dropdown/lib/item-quantity-dropdown.min.js'
+import '../node_modules/item-quantity-dropdown/lib/item-quantity-dropdown.min.css'
 // ---PLUGINS END---
 
 
--- src/js/form-elements.js
+++ src/js/form-elements.js
@@ -2,13 +2,41 @@ $(document).ready(function(){
   // Mask for input
   $(".date").mask("99.99.9999",{placeholder:"ДД.ММ.ГГГГ"});
 
-  // Date start - end
+  // Date start - end, 
   $('#date-start').datepicker({
+    range: true,
+    multipleDatesSeparator: '-',
+    clearButton: true,
     onSelect: function (date) {
       $('#date-start').val(date.split("-")[0]);
       $('#date-end').val(date.split("-")[1]);
     }
   });
+  
+  //  translate date in filter-date-dropdown
+  $('#date-filter').datepicker({
+    range: true,
+    multipleDatesSeparator: ' - ',
+    dateFormat: 'dd M'
+  })
 
+  $('.iqdropdown').iqDropdown({
+    maxItems: 9,
+    minItems: 0,
+    selectionText: 'Гость',
+    textPlural: 'Гостей',
+    controls: {
+      position: 'right',
+      displayCls: 'iqdropdown-item-display',
+      controlsCls: 'iqdropdown-item-controls',
+      counterCls: 'counter'
+    },
+    // fires when an item quantity changes
+    onChange: (id, count, totalItems) => {},
+    // return false to prevent an item decrement
+    beforeDecrement: (id, itemCount) => {},
+    // return false to prevent an item increment
+    beforeIncrement: (id, itemCount) => {}
+  });
 });
 
