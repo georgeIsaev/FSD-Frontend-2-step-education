12faa48e199e00fdd44a75a28c6871e62d3c547e
Gevorg <georgeisaev7@gmail.com>
2019-08-26 19:41:55 +0400

    Add mixins to create all buttons and pagination(not complete)

--- src/js/form-elements.js
+++ src/js/form-elements.js
@@ -296,7 +296,7 @@ function openCheckboxList () {
   let cbOpener = $('.isExpandable').find('.form-element-caption')
   let hiddenList = $('.isExpandable').find('.expandable-checkbox-menu')
   cbOpener.click((event) => {
-    if(hiddenList.css('display') == 'none'){
+    if (hiddenList.css('display') == 'none'){
       hiddenList.css('display', 'block')
       cbOpener.find('.glyphicon').css({
         'transform': 'rotate(135deg)',
@@ -323,7 +323,7 @@ function likeBtn (ths) {
   let likeCount = parseInt(thisLikeBtn.find('.likes-count').html())
 
   thisLikeBtn.toggleClass('liked')
-  if(thisLikeBtn.hasClass('liked')){
+  if (thisLikeBtn.hasClass('liked')){
     thisLikeBtn.find('.likes-count').html(likeCount + 1)
   } else {
     thisLikeBtn.find('.likes-count').html(likeCount - 1)
@@ -334,3 +334,81 @@ $('.like-button').click((event) => {
   likeBtn(event.target)
   event.preventDefault()
 })
+
+
+//Pagination
+let dataPages = $('.pagination-btn').find('a')
+
+function pagination (){
+  for (let i = 0; i < dataPages.length - 1; i++){
+
+    $(dataPages).css("display", "none")
+    $('.pagination-btn').find('.ellipsis').remove()
+    $('.start-page').css("display", "flex")
+    $('.final-page').css("display", "flex")
+    $('.next-page').css({
+      "display": "flex",
+      "opacity": "1"
+    })
+    
+    if (($(dataPages[i]).hasClass('active-page') && $(dataPages[i]).hasClass('start-page')) || $(dataPages[1]).hasClass('active-page')){
+      $(dataPages[1]).css("display", "flex")
+      $(dataPages[2]).css("display", "flex")
+      $(dataPages[i+2]).after('<i class="ellipsis">...</i>')
+      break
+    } else if ($(dataPages[2]).hasClass('active-page')){
+      $(dataPages[1]).css("display", "flex")
+      $(dataPages[2]).css("display", "flex")
+      $(dataPages[3]).css("display", "flex")
+      $(dataPages[3]).after('<i class="ellipsis">...</i>')
+      break
+    } else if (($(dataPages[i]).hasClass('active-page') && $(dataPages[i]).hasClass('final-page')) || $(dataPages[dataPages.length - 3]).hasClass('active-page')){
+      if($(dataPages[i]).hasClass('final-page')){
+        $('.next-page').css("opacity", "0.5")
+      }
+      $(dataPages[dataPages.length - 3]).css("display", "flex")
+      $(dataPages[dataPages.length - 4]).css("display", "flex")
+      $(dataPages[dataPages.length - 4]).before('<i class="ellipsis">...</i>')
+      break
+    } else if ($(dataPages[dataPages.length - 4]).hasClass('active-page')){
+      $(dataPages[dataPages.length - 3]).css("display", "flex")
+      $(dataPages[dataPages.length - 4]).css("display", "flex")
+      $(dataPages[dataPages.length - 5]).css("display", "flex")
+      $(dataPages[dataPages.length - 5]).before('<i class="ellipsis">...</i>')
+      break
+    } else if ($(dataPages[i]).hasClass('active-page')){
+      $(dataPages[i - 1]).css("display", "flex")
+      $(dataPages[i + 1]).css("display", "flex")
+      $(dataPages[i]).css("display", "flex")
+      $(dataPages[i + 1]).after('<i class="ellipsis">...</i>')
+      $(dataPages[i - 1]).before('<i class="ellipsis">...</i>')
+      break
+    }
+  }
+}
+
+pagination()
+for (let i = 0; i < dataPages.length; i++){
+  if (i < dataPages.length - 1) {
+    $(dataPages[i]).click((event) => {
+      $('.active-page').removeClass('active-page')
+      $(dataPages[i]).addClass('active-page')
+      event.preventDefault()
+      pagination()
+    })
+  } else {
+    $(dataPages[i]).click((event) => {
+      for (let j = 0; j < dataPages.length - 1; j++){
+        if ($(dataPages[j]).hasClass('active-page') && j < dataPages.length - 2){
+          $(dataPages[j]).removeClass('active-page')
+          $(dataPages[j + 1]).addClass('active-page')
+          break
+        }
+      }
+      event.preventDefault()
+      pagination()
+    })
+  }
+}
+
+
