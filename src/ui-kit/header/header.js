786ef5c4ba7e47c2cd529d992d5af62a26977d96
George <georgeisaev7@gmail.com>
2019-09-09 13:08:44 +0300

    Added mixin to create footer, updated header's mixin

--- src/ui-kit/header/header.js
+++ src/ui-kit/header/header.js
@@ -1,13 +1,16 @@
 $(document).ready(() => {
   let menuCollapseOpener = $('.menu').find('.menu__header').find('.menu__header__toggle')
   let menuCollapsedList = $('.menu').find('.menu__list')
+  let placeForMenu = $('.place-for-menu')
 
   menuCollapseOpener.click((event) => {
     if (menuCollapsedList.css('top') != '70px') {
+      placeForMenu.css('display', 'block')
       menuCollapsedList.css({
         'top': '70px'
       })
     } else {
+      placeForMenu.css('display', 'none')
       menuCollapsedList.css({
         'top': '-99999px'
       })
