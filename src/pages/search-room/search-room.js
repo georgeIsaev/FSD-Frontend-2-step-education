d8897d0c9758d45af35432be5755cf958a7ddc17
George <georgeisaev7@gmail.com>
2019-09-13 20:16:51 +0300

    Created search-room.html pages, added responsive for section with rooms-card

---
+++ src/pages/search-room/search-room.js
@@ -0,0 +1,27 @@
+import '../../ui-kit/header/header'
+import '../../ui-kit/form-elements/filter-date-dropdown/filter-date-dropdown'
+import '../../ui-kit/form-elements/quantity-dropdown/quantity-dropdown'
+import '../../ui-kit/form-elements/range-slider/range slider'
+import '../../ui-kit/form-elements/checkbox-list/checkbox-list'
+import '../../ui-kit/form-elements/pagination/pagination'
+import '../../ui-kit/cards/room-card/room-card'
+
+$(document).ready(() => {
+  let filtersBtnOpen = $('._search-room').find('.filters__btn._open')
+  let filtersBtnApply = $('._search-room').find('.filters__btn._apply')
+  let filters = $('._search-room').find('.filters')
+
+  filtersBtnOpen.click((event) => {
+    filters.css('display', 'block')
+    filtersBtnApply.css('display', 'block')
+    filtersBtnOpen.css('display', 'none')
+    event.preventDefault()
+  })
+
+  filtersBtnApply.click((event) => {
+    filters.css('display', 'none')
+    filtersBtnApply.css('display', 'none')
+    filtersBtnOpen.css('display', 'block')
+    event.preventDefault()
+  })
+})
